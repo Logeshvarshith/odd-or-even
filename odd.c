d9560759b9b3a64ffbc94077740f3ad61e9d68ed
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-11 16:01:14 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value of a");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("%d is an even num");
+}
+else
+{
+printf("%d is an odd num");
+}
+getch();
+}
