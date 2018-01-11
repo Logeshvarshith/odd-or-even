5b837cb3e977f0407ddea24a0603331b750adfba
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-11 16:12:54 +0530

    Create biggest.java

---
+++ biggest.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class big
+{
+public static void main(strings args[])
+{
+intb a=5;
+int b=10,c=15;
+if(a<b)
+{
+if(b<c)
+{
+system.out.print/n("c is biggest",+c);
+}
+else
+{
+system.out.print/n("b is biggest",+b);
+}
+else
+{
+system.out.print/n("a is biggest",+a);
+}
+}}}
