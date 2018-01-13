b96423dfb2d4d7eabf6ed9366cd8ecabed915825
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-13 09:33:57 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,r=0,digit,num;
+printf("enter num");
+n=num;
+while(n!=0)
+{
+r=n%10
+rev=(rev*10)+r;
+num=n/10;
+if(n=rev)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+}
