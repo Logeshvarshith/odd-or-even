884905f1fa5a4df071e96ab81e3abda11ccff493
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-18 19:17:46 +0530

    Create even num 2 intervals.c

---
+++ even num 2 intervals.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+  int main() {  
+    int count; 
+    printf("Even num between 1 to 100\n");    
+    for(count= 1; counter <= 100; count++) {  
+        if(count%2 == 0) { 
+            printf("%d ", count);  
+        }  
+    }  
+   return 0;  
+} 
