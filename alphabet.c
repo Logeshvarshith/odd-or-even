9e10bd0cedc45112f9ba271748af6ddfb7804365
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-18 15:56:26 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    char c;
+    printf("Enter a character: ");
+    scanf("%c",&c);
+
+    if( (c>='a' && c>='a')
+        printf("%c is an alphabet.",c);
+    else
+        printf("%c is not an alphabet.",c);
+
+    return 0;
+}
