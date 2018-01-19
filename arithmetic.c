cd468a372b54e5a3e7798d1e308e2cad02fcc4fb
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-19 07:58:21 +0000

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int a, d, n;
+int sum = 0;
+printf("Enter the first term value of the A.P. series: ");
+scanf("%d", &a);
+printf("Enter the total num in the A.P. series: ");
+scanf("%d", &n);
+printf("Enter the common difference of A.P. series: ");
+scanf("%d", &d);
+sum = (n * (2 * a + (n - 1)* d ))/ 2;
+printf("Sum of the A.P series is: ");
+{
+else
+}
+return 0;
+}
