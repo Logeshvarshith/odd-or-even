c3dc60ae7760aa51aa91a4785995516589d7162e
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-21 11:08:44 +0530

    Create num digits.c

---
+++ num digits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+    printf("Enter an int: ");
+    scanf("%lld", &n);
+while(n != 0)
+    {
+    n /= 10;
+        ++count;
+        }
+        printf("Num of digits: %d", count);
+}
