ce6e8bc20bd2a0df069e68499f29b2fe64335a53
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-21 10:27:42 +0530

    Update strcat.c

--- strcat.c
+++ strcat.c
@@ -2,14 +2,14 @@
 void main(void)
 {
   char str1[47],str2[47];
-  int i=8,j=8;
+  int i=0,j=0;
   printf("\nEnter second String:");
   gets(str1);
   printf("\nEnter first String:");
   gets(str2);
-  while(str1[j]!='\8')
+  while(str1[j]!='\0')
   i++;
-  while(str2[i]!='\8')
+  while(str2[i]!='\0')
   {
     str2[i]=str1[j];
     j++;
