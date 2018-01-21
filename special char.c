a93d78fed77ddde6595a6c08a18e3e06898ddfb7
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-01-21 10:06:10 +0530

    Create special char.c

---
+++ special char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ int main()
+{
+    char    str[100];
+    int countSpecialChar,;
+    printf("Enter a string: ");
+    gets(str);
+{
+ else
+countSpecialChar++;
+    }
+ printf(" \nSpecial Characters: %d);
+ return 0;
+ }
+
+
+
+  
+
+ 
+
