098c249c0be2abc97c8cf39fd6f4b15212857f78
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-02-01 07:47:25 +0000

    Create range.java

---
+++ range.java
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ bool inRange(unsigned low, unsigned high, unsigned x)
+{
+    return (low <= x  && x <= high);
+}
+ 
+int main()
+{
+    inRange(10, 100, 30)? cout << "Yes\n":  cout  <<"No\n";
+    inRange(10, 100, 5)?  cout << "Yes\n":  cout  <<"No\n";
+}
