583d7543b70554f4698c53e55572b1e81e8bec52
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-02-02 09:02:09 +0000

    Update pali.c

--- pali.c
+++ pali.c
@@ -2,29 +2,29 @@
 #include<string.h>
 int main(void)
 {
-	char a[100];
-	int i,j,count=0;
+	char i[300];
+	int a,b,count=0;
 	printf("\nEnter the string : ");
-	scanf("%s",&a);
-	i=0;
-	j=strlen(a);
-	while(a[i]!='\0')
+	scanf("%s",&i);
+	a=0;
+	b=strlen(i);
+	while(i[a]!='\0')
 	{
-		if(a[i]==a[j-1]&&a[i]!='\0')
+		if(i[a]==i[b-1]&&i[a]!='\0')
 		{
-		i++;
-		j--;
+		a++;
+		b--;
 		count++;
 		}
 		else
 		{
-			printf("\n%s is not a palindrome.",a);
+			printf("\n%s is not a palindrome.",i);
 			break;
 		}
 	}
-	if(count==strlen(a))
+	if(count==strlen(i))
 	{
-		printf("\n%s is the palindrome.",a);
+		printf("\n%s is the palindrome.",i);
 	}
 	return 0;
 }
